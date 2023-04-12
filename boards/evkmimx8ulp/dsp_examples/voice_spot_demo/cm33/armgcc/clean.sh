34e70293f562c081b8d165230cdb94034ff901f9
wenxue.guo <wenxue.guo@avnet.com>
2023-04-12 18:00:02 +0800

    Initial SDK_2_13_1_EVK-MIMX8ULP from mcuxpresso.nxp.com Convert rpmsg_lite_str_echo_rtos source code from windows to unix format
    Signed-off-by: wenxue.guo <wenxue.guo@avnet.com>

---
+++ boards/evkmimx8ulp/dsp_examples/voice_spot_demo/cm33/armgcc/clean.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+rm -rf release flash_release CMakeFiles
+rm -rf Makefile cmake_install.cmake CMakeCache.txt
---
+++ boards/evkmimx8ulp/dsp_examples/voice_spot_demo/cm33/armgcc/flags.cmake
@@ -0,0 +1,184 @@
+IF(NOT DEFINED FPU)  
+    SET(FPU "-mfloat-abi=hard -mfpu=fpv5-sp-d16")  
+ENDIF()  
+
+IF(NOT DEFINED SPECS)  
+    SET(SPECS "--specs=nano.specs --specs=nosys.specs")  
+ENDIF()  
+
+IF(NOT DEFINED DEBUG_CONSOLE_CONFIG)  
+    SET(DEBUG_CONSOLE_CONFIG "-DSDK_DEBUGCONSOLE=1")  
+ENDIF()  
+
+SET(CMAKE_ASM_FLAGS_FLASH_RELEASE " \
+    ${CMAKE_ASM_FLAGS_FLASH_RELEASE} \
+    -DDSP_IMAGE_COPY_TO_RAM=1 \
+    -DNDEBUG \
+    -D__STARTUP_CLEAR_BSS \
+    -D__STARTUP_INITIALIZE_NONCACHEDATA \
+    -mcpu=cortex-m33 \
+    -mthumb \
+    ${FPU} \
+")
+SET(CMAKE_ASM_FLAGS_RELEASE " \
+    ${CMAKE_ASM_FLAGS_RELEASE} \
+    -DDSP_IMAGE_COPY_TO_RAM=1 \
+    -DNDEBUG \
+    -D__STARTUP_CLEAR_BSS \
+    -D__STARTUP_INITIALIZE_NONCACHEDATA \
+    -mcpu=cortex-m33 \
+    -mthumb \
+    ${FPU} \
+")
+SET(CMAKE_C_FLAGS_FLASH_RELEASE " \
+    ${CMAKE_C_FLAGS_FLASH_RELEASE} \
+    -DDSP_IMAGE_COPY_TO_RAM=1 \
+    -DNDEBUG \
+    -DFSL_SDK_DRIVER_QUICK_ACCESS_ENABLE=1 \
+    -DENABLE_RAM_VECTOR_TABLE \
+    -DCPU_MIMX8UD7DVP10_cm33 \
+    -DSDK_I2C_BASED_COMPONENT_USED=1 \
+    -DBOARD_USE_CODEC=1 \
+    -DCODEC_WM8960_ENABLE \
+    -DDEBUG_CONSOLE_RX_ENABLE=0 \
+    -DSERIAL_PORT_TYPE_UART=1 \
+    -DMCUXPRESSO_SDK \
+    -O2 \
+    -mcpu=cortex-m33 \
+    -Wall \
+    -mthumb \
+    -MMD \
+    -MP \
+    -fno-common \
+    -ffunction-sections \
+    -fdata-sections \
+    -ffreestanding \
+    -fno-builtin \
+    -mapcs \
+    -std=gnu99 \
+    ${FPU} \
+    ${DEBUG_CONSOLE_CONFIG} \
+")
+SET(CMAKE_C_FLAGS_RELEASE " \
+    ${CMAKE_C_FLAGS_RELEASE} \
+    -DDSP_IMAGE_COPY_TO_RAM=1 \
+    -DNDEBUG \
+    -DCPU_MIMX8UD7DVP10_cm33 \
+    -DSDK_I2C_BASED_COMPONENT_USED=1 \
+    -DBOARD_USE_CODEC=1 \
+    -DCODEC_WM8960_ENABLE \
+    -DDEBUG_CONSOLE_RX_ENABLE=0 \
+    -DSERIAL_PORT_TYPE_UART=1 \
+    -DMCUXPRESSO_SDK \
+    -Os \
+    -mcpu=cortex-m33 \
+    -Wall \
+    -mthumb \
+    -MMD \
+    -MP \
+    -fno-common \
+    -ffunction-sections \
+    -fdata-sections \
+    -ffreestanding \
+    -fno-builtin \
+    -mapcs \
+    -std=gnu99 \
+    ${FPU} \
+    ${DEBUG_CONSOLE_CONFIG} \
+")
+SET(CMAKE_CXX_FLAGS_FLASH_RELEASE " \
+    ${CMAKE_CXX_FLAGS_FLASH_RELEASE} \
+    -DNDEBUG \
+    -DSERIAL_PORT_TYPE_UART=1 \
+    -DMCUXPRESSO_SDK \
+    -Os \
+    -mcpu=cortex-m33 \
+    -Wall \
+    -mthumb \
+    -MMD \
+    -MP \
+    -fno-common \
+    -ffunction-sections \
+    -fdata-sections \
+    -ffreestanding \
+    -fno-builtin \
+    -mapcs \
+    -fno-rtti \
+    -fno-exceptions \
+    ${FPU} \
+    ${DEBUG_CONSOLE_CONFIG} \
+")
+SET(CMAKE_CXX_FLAGS_RELEASE " \
+    ${CMAKE_CXX_FLAGS_RELEASE} \
+    -DNDEBUG \
+    -DSERIAL_PORT_TYPE_UART=1 \
+    -DMCUXPRESSO_SDK \
+    -Os \
+    -mcpu=cortex-m33 \
+    -Wall \
+    -mthumb \
+    -MMD \
+    -MP \
+    -fno-common \
+    -ffunction-sections \
+    -fdata-sections \
+    -ffreestanding \
+    -fno-builtin \
+    -mapcs \
+    -fno-rtti \
+    -fno-exceptions \
+    ${FPU} \
+    ${DEBUG_CONSOLE_CONFIG} \
+")
+SET(CMAKE_EXE_LINKER_FLAGS_FLASH_RELEASE " \
+    ${CMAKE_EXE_LINKER_FLAGS_FLASH_RELEASE} \
+    -mcpu=cortex-m33 \
+    -Wall \
+    -fno-common \
+    -ffunction-sections \
+    -fdata-sections \
+    -ffreestanding \
+    -fno-builtin \
+    -mthumb \
+    -mapcs \
+    -Xlinker \
+    --gc-sections \
+    -Xlinker \
+    -static \
+    -Xlinker \
+    -z \
+    -Xlinker \
+    muldefs \
+    -Xlinker \
+    -Map=output.map \
+    -Wl,--print-memory-usage \
+    ${FPU} \
+    ${SPECS} \
+    -T${ProjDirPath}/MIMX8UD7xxxxx_cm33_flash.ld -static \
+")
+SET(CMAKE_EXE_LINKER_FLAGS_RELEASE " \
+    ${CMAKE_EXE_LINKER_FLAGS_RELEASE} \
+    -mcpu=cortex-m33 \
+    -Wall \
+    -fno-common \
+    -ffunction-sections \
+    -fdata-sections \
+    -ffreestanding \
+    -fno-builtin \
+    -mthumb \
+    -mapcs \
+    -Xlinker \
+    --gc-sections \
+    -Xlinker \
+    -static \
+    -Xlinker \
+    -z \
+    -Xlinker \
+    muldefs \
+    -Xlinker \
+    -Map=output.map \
+    -Wl,--print-memory-usage \
+    ${FPU} \
+    ${SPECS} \
+    -T${ProjDirPath}/MIMX8UD7xxxxx_cm33_ram.ld -static \
+")
